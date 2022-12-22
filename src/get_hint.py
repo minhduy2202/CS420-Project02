31cc64947f5b6abdf21f65ab74488bf8f316f6cf
Le Van Hoang Phi <phipk02@gmail.com>
2022-12-22 17:20:54 +0700

    [update] get_hint

--- src/get_hint.py
+++ src/get_hint.py
@@ -1,12 +1,14 @@
 import random
 
-def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map: list, _weights: list) -> tuple:
+def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: tuple, _pirateLocation: tuple, _agentLocation: tuple, _map: list, _weights: list) -> tuple:
     """
     *args:
         @_w: Weight.
         @_h: Height.
         @_numOfRegions: Number of regions that the map has.
         @_treasureLocation: The location of treasure.
+        @_pirateLocation: The location of pirate.
+        @_agentLocation: The location of agent.
         @_map: The game map.
         @_weights: A list Of N (N is the number Of hints) weights to random hint.
     
@@ -24,23 +26,35 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
         numOfTiles = random.randrange(int(_w * _h / 8) + 1)
         listOfTiles = random.sample(range(_w * _h), numOfTiles)
         
-        isTruth = (hint == 1) ^ (_treasureLocation[0] * _treasureLocation[1] in listOfTiles)
+        isTruth = (hint == 1) ^ ((_treasureLocation[1] + _w * _treasureLocation[0]) in listOfTiles)
         
         return (hint, listOfTiles, isTruth, f"A list of tiles {listOfTiles} " + ("does not contain the treasure." if hint == 1 else "contains the treasure."))
     elif hint == 3: # "? regions that 1 of them has the treasure."
         numOfRegions = random.randrange(2, min(_numOfRegions, 6))
         listOfRegions = random.sample(range(1, _numOfRegions), numOfRegions)
         
-        isTruth = int(_map[_treasureLocation[0], _treasureLocation[1]][0]) in listOfRegions
+        listOfTiles = []
         
-        return (hint, listOfRegions, isTruth, f"One of region in {listOfRegions} has the treasure.")
+        for i in range(_w):
+            for j in range(_h):
+                if int(_map[i][j][0]) in listOfRegions: listOfTiles.append(i * _w + j)
+        
+        isTruth = (_treasureLocation[1] + _w * _treasureLocation[0]) in listOfTiles
+        
+        return (hint, listOfTiles, isTruth, f"One of region in {listOfRegions} has the treasure.")
     elif hint == 4: # "? regions that do not contain the treasure."
         numOfRegions = random.randrange(1, min(_numOfRegions, 4))
         listOfRegions = random.sample(range(1, _numOfRegions), numOfRegions)
         
-        isTruth = int(_map[_treasureLocation[0], _treasureLocation[1]][0]) not in listOfRegions
+        listOfTiles = []
         
-        return (hint, listOfRegions, isTruth, f"All regions in {listOfRegions} do not have the treasure.")
+        for i in range(_w):
+            for j in range(_h):
+                if int(_map[i][j][0]) in listOfRegions: listOfTiles.append(i * _w + j)
+        
+        isTruth = (_treasureLocation[1] + _w * _treasureLocation[0]) not in listOfTiles
+        
+        return (hint, listOfTiles, isTruth, f"All regions in {listOfRegions} do not have the treasure.")
     elif hint == 5: # "A rectangle area ? that has the treasure.",
         x1 = random.randrange(int(_w / 4)  + 1)
         y1 = random.randrange(int(_h / 4)  + 1)
@@ -48,8 +62,13 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
         y2 = random.randrange(y1 + int(_h * 3 / 8), _h)
         
         isTruth = (x1 <= _treasureLocation[0] and _treasureLocation[0] <= x2) and (y1 <= _treasureLocation[1] and _treasureLocation[1] <= y2)
+        listOfTiles = []
         
-        return (hint, [x1, y1, x2, y2], isTruth, f"There is treasure in the rectangle area {[x1, y1, x2, y2]}.")
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                listOfTiles.append(i * _w + j)
+        
+        return (hint, listOfTiles, isTruth, f"There is treasure in the rectangle area {[x1, y1, x2, y2]}.")
     elif hint == 6: # "A rectangle area ? that doesn't have the treasure.",
         x1 = random.randrange(_w - 4)
         y1 = random.randrange(_h - 4)
@@ -58,10 +77,23 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
         
         isTruth = ~((x1 <= _treasureLocation[0] and _treasureLocation[0] <= x2) and (y1 <= _treasureLocation[1] and _treasureLocation[1] <= y2))
         
-        return (hint, [x1, y1, x2, y2], isTruth, f"There is no treasure in the rectangle area {[x1, y1, x2, y2]}.")
+        listOfTiles = []
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                listOfTiles.append(i * _w + j)
+        
+        return (hint, listOfTiles, isTruth, f"There is no treasure in the rectangle area {[x1, y1, x2, y2]}.")
     elif hint == 7: # "The pirate tells that you are the nearest person to the treasure.",
-        # cai hint nay dan qua, de tinh sau.
-        pass
+        listOfTiles = []
+        
+        isTruth = (abs(_treasureLocation[0] - _agentLocation[0]) + abs(_treasureLocation[1] - _agentLocation[1])) < (abs(_treasureLocation[0] - _pirateLocation[0]) + abs(_treasureLocation[1] - _pirateLocation[1]))
+        
+        for i in range(_w):
+            for j in range(_h):
+                if (abs(i - _agentLocation[0]) + abs(j - _agentLocation[1])) < (abs(i - _pirateLocation[0]) + abs(j - _pirateLocation[1])):
+                    listOfTiles.add(i * _w + j)
+                    
+        return (hint, listOfTiles, isTruth, "The pirate tells that you are the nearest person to the treasure.")
     elif hint == 8: # "A column and/or a row that contain the treasure." (rare),
         row, column = False, False
         
@@ -75,17 +107,25 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
         
         isTruth = False
         
+        listOfTiles = []
+        
         if row and column: 
             s = f"Column {c} and row {r} contain the treasure."
             isTruth = (r == _treasureLocation[0]) and (c == _treasureLocation[1])
+            
+            listOfTiles.append(r * _w + c)
         elif row: 
             s = f"Row {r} contains the treasure."
             isTruth = (r == _treasureLocation[0])
+            
+            for j in range(_h): listOfTiles.append(r  * _w + j)
         else: 
             s = f"Column {c} contains the treasure."
             isTruth = (c == _treasureLocation[1])
+            
+            for i in range(_w): listOfTiles.append(i * _w + c)
         
-        return (hint, [r, c], isTruth, s)
+        return (hint, listOfTiles, isTruth, s)
     elif hint == 9: # "A column and/or a row that don't contain the treasure.",
         row, column = False, False
         
@@ -99,17 +139,24 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
         
         isTruth = False
         
+        listOfTiles = []
+        
         if row and column: 
             s = f"Column {c} and row {r} do not contain the treasure."
             isTruth = (r != _treasureLocation[0]) or (c != _treasureLocation[1])
+            listOfTiles.append(r * _w + c)
         elif row: 
             s = f"Row {r} does not contain the treasure."
             isTruth = (r != _treasureLocation[0])
+            
+            for j in range(_h): listOfTiles.append(r  * _w + j)
         else: 
             s = f"Column {c} does not contain the treasure."
             isTruth = (c != _treasureLocation[1])
             
-        return (hint, [r, c], isTruth, s)
+            for i in range(_w): listOfTiles.append(i * _w + c)
+            
+        return (hint, listOfTiles, isTruth, s)
     elif hint == 10: # "The treasure is somewhere in the boundary of 2 regions ?.",
         adjacent = [(-1, 0), (1, 0), (0, 1), (0, - 1)]
         regionPairs = dict()
@@ -128,13 +175,13 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
                             if region_1 > region_2:
                                 region_1, region_2 = region_2, region_1
                             if (region_1, region_2) not in regionPairs.keys():
-                                regionPairs[(region_1, region_2)] = [(i, j)]
+                                regionPairs[(region_1, region_2)] = [j+ i * _w ]
                             else:
-                                regionPairs[(region_1, region_2)].append((i, j))
+                                regionPairs[(region_1, region_2)].append(j + i * _w)
         
         regionPair, listOfTiles = random.choice(list(regionPairs.items()))
         
-        isTruth = tuple(_treasureLocation) in listOfTiles
+        isTruth = (_treasureLocation[1] + _treasureLocation[0] * _w) in listOfTiles
         
         return (hint, set(listOfTiles), isTruth, f"The treasure is somewhere in the boundary of 2 regions {regionPair}")
     elif hint == 11: # "The treasure is somewhere in a boundary of 2 regions.",
@@ -152,9 +199,9 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
                             continue
                         
                         if region_1 != region_2:
-                            setOfTiles.add((i, j))
+                            setOfTiles.add(j + i * _w)
         
-        isTruth = tuple(_treasureLocation) in setOfTiles
+        isTruth = (_treasureLocation[0] * _w +_treasureLocation[1]) in setOfTiles
         
         for r in _map:
             print(r)
@@ -177,18 +224,125 @@ def genHint(_w: int, _h: int, _numOfRegions: int, _treasureLocation: list, _map:
                 if _map[i][j] == "0":
                     for tup in bounded:
                         if i + tup[0] >= 0 and i + tup[0] < _w and j + tup[1] >= 0 and j + tup[1] < _h:
-                            setOfTiles.add((i + tup[0], j + tup[1]))
+                            setOfTiles.add((i + tup[0]) * _w + (j + tup[1]))
                             
-        isTruth = tuple(_treasureLocation) in setOfTiles
+        isTruth = (_treasureLocation[0] * _w + _treasureLocation[1]) in setOfTiles
         
         return (hint, setOfTiles, isTruth, f"The treasure is somewhere in an area bounded by {sizeOfBoundedArea} tiles from sea.")
     elif hint == 13: # A half of the map without treasure (rare)
-        pass
-    elif hint == 14: # (From the center Of the map)/(from the prison) that he's staying, he tells you a direction that has the treasure (W, E, N, S or SE, SW, NE, NW), the shape Of area when the hints are (W, E, N, S) is triangle
-        pass
+        direction = random.randrange(4)
+        
+        if direction == 0: # South
+            r = int(_w / 2)
+            
+            isTruth = ~(_treasureLocation < (r, _h - 1))
+            
+            return (hint, [0, 0, r - 1, _h - 1], isTruth, f"Half up side of the map does not have the treasure.")
+        if direction == 1: # North
+            r = int(_w / 2)
+            
+            isTruth = ~(r <= _treasureLocation[0] and r < _treasureLocation[0] and _treasureLocation[1] < _h)
+            
+            return (hint, [r, 0, _w - 1, _h - 1], isTruth, f"Half down side of the map does not have the treasure.")
+        if direction == 2: # West
+            c = int(_h / 2)
+            
+            isTruth = ~(_treasureLocation < (_w - 1, c))
+            
+            return (hint, [0, 0, _w - 1, c - 1], isTruth, f"Half left side of the map does not have the treasure.")
+        if direction == 3: # East
+            c = int(_h / 2)
+            
+            isTruth = ~(c <= _treasureLocation[1] and c < _treasureLocation[1] and _treasureLocation[0] < _w)
+            
+            return (hint, [0, c, _w - 1, _h - 1], isTruth, f"Half right side of the map does not have the treasure.")
+    elif hint == 14: # (From the center of the map)/(from the prison that he's staying), he tells you a direction that has the treasure (W, E, N, S or SE, SW, NE, NW), the shape Of area when the hints are (W, E, N, S) is triangle
+        choice = random.randrange(2)
+        
+        x, y = 0, 0
+        if choice == 0:
+            x, y = int(_w / 2), int(_h / 2)                
+        else:
+            x, y = _pirateLocation   
+            
+        setOfTiles = set()     
+        
+        direction = random.randrange(8)
+        d = ['E', 'W', 'N', 'S', 'SE', 'SW', 'NE', 'NW']
+        
+        if direction in [0, 1]: # East and West
+            l, r = x, x
+            while (direction == 0 and y < _h) or (direction == 1 and y >= 0):
+                for i in range(max(l, 0), min(r, _w)):
+                    setOfTiles.add(y + i * _w)
+                l -= 1
+                r += 1
+                y += (-1 if direction == 1 else 1)
+        if direction in [2, 3]: # South and North
+            l, r = y, y
+            while (direction == 2 and x < _w) or (direction == 3 and x >= 0):
+                for i in range(max(l, 0), min(r, _h)):
+                    setOfTiles.add(i + x * _w)
+                l -= 1
+                r += 1
+                x += (-1 if direction == 3 else 1)
+        if direction in [4, 5, 6, 7]:
+            xl, yl, xr, yr = 0, 0, _w - 1, _h - 1
+        
+            if direction == 4: # SE
+                xl, yl = x, y
+            if direction == 5: # SW
+                xl, yr = x, y
+            if direction == 6: # NE
+                xr, yl = x, y
+            if direction == 7: # NW
+                xr, yr = x, y
+            
+            for i in range(xl, xr + 1):
+                for j in range(yl, yr + 1):
+                    setOfTiles.add(j + i * _w)
+                
+        isTruth = (_treasureLocation[0] * _w + _treasureLocation[1]) in setOfTiles
+            
+        return (hint, setOfTiles, isTruth, ("From center of the map" if choice == 0 else "From the prison that the pirate's staying") + f", he tells you that direction {d[direction]} has the treasure.")
     elif hint == 15: # 2 squares that are different in size, the small one is placed inside the bigger one, the treasure is somewhere inside the gap between 2 squares. (rare)
-        pass
+        x1 = random.randrange(2, _w - 4)
+        y1 = random.randrange(2, _h - 4)
+        x2 = random.randrange(x1 + 1, x1 + 3)
+        y2 = random.randrange(y1 + 1, y1 + 3)
+        
+        x3 = random.randrange(x1)
+        x4 = random.randrange(x2 + 1, _w)
+        y3 = random.randrange(y1)
+        y4 = random.randrange(y2 + 1, _h)
+        
+        setOfTiles = set()
+        
+        for i in range(x3, x4 + 1):
+            for j in range(y3, y4 + 1):
+                if i in (x1, x2 + 1) and j in range(y1, y2 + 1): continue
+                setOfTiles.add(j + i * _w)
+        
+        isTruth = (_treasureLocation[0] * _w + _treasureLocation[1]) in setOfTiles
+        
+        return (hint, setOfTiles, isTruth, f"The treasure is somewhere inside the gap between {[x1, y1, x2, y2]} and {[x3, y3, x4, y4]}.")
     else: # The treasure is in a region that has mountain.
-        pass
+        _temp = dict()
+        regions = set()
+        
+        for i in range(_w):
+            for j in range(_h):
+                if len(_map[i][j] > 1) and _map[i][j][1] == 'M': regions.add(_map[i][j][0])
+                
+                if _map[i][j][0] not in _temp.keys(): _temp[_map[i][j][0]] = [i * _w + j]
+                else: _temp[_map[i][j][0]].append(i * _w + j)
+        
+        listOfTiles = []
+        for r in regions:
+            listOfTiles = listOfTiles + _temp[r]
+            
+        isTruth = (_treasureLocation[0] * _w + _treasureLocation[1]) in listOfTiles
+        
+        return (hint, listOfTiles, isTruth, "The treasure is in a region that has mountain.")
 
-print(genHint(5, 5, 3, [3, 4], [['0', '0', '0', '0', '0'], ['0', '1', '1M', '1P', '1'], ['0', '1P', '2M', '2', '2'], ['0', '3', '3M', '3', '3'], ['0', '0', '0', '0', '0']], []))
+print(genHint(5, 5, 3, (3, 4), (1, 2), (5, 5), [['0', '0', '0', '0', '0'], ['0', '1', '1M', '1P', '1'], ['0', '1P', '2M', '2', '2'], ['0', '3', '3M', '3', '3'], ['0', '0', '0', '0', '0']], []))
