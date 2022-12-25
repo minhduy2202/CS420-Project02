d8b79fe746cbdd309bc8aa356bf49b56878e7a9c
Le Van Hoang Phi <phipk02@gmail.com>
2022-12-26 01:58:08 +0700

    aaa

--- src/main.py
+++ src/main.py
@@ -52,6 +52,17 @@ if __name__ == '__main__':
         round = 1
         kBase = []
         hints = []
+        removedTiles = set()
         
         while True:
-            
+            if round == revealRound:
+                # adjust hint probability
+                pass
+            if round == freeRound:
+                # free pirate
+                pass
+            # getHint
+            # getActions
+            # applyActions
+            # Pirate moves if free
+            # check finish
