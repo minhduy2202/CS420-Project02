518ddb8d5dc56878e13dba8dfc989424647aa281
Le Van Hoang Phi <phipk02@gmail.com>
2022-12-26 11:40:21 +0700

    [update] genMp

--- src/genMap.py
+++ src/genMap.py
@@ -9,27 +9,26 @@ def generate_island_map(h, w, k):
     start_row = random.randint(0, h - 1)
     start_col = random.randint(0, w - 1)
     
-    while grid[start_row][start_col]!= 0: #(start_row, start_col) in processed_cells and 
+    while grid[start_row][start_col] != 0: #(start_row, start_col) in processed_cells and 
         start_row = random.randint(0, h - 1)
         start_col = random.randint(0, w - 1)
     queue = [(start_row, start_col)]
     
+    processed_cells.add((start_row, start_col))
+    
     p = random.randint(int(w * h / (k + 1)), int(w * h / k))
     while queue:
-      row, col = queue.pop(0)
-      
-      # Mark cell as processed
-      processed_cells.add((row, col))
-      
-      # Set cell value to i + 1 (land)
-      grid[row][col] = i + 1
-      
-      for r, c in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
-        if 0 <= r < h and 0 <= c < w and (r, c) not in processed_cells:
-          if p:
-            queue.append((r, c))
-            p -= 1
-  
+        row, col = queue.pop(0)
+
+        grid[row][col] = i + 1
+        
+        for r, c in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
+            if 0 <= r < h and 0 <= c < w and (r, c) not in processed_cells:
+                if p:
+                    processed_cells.add((r, c))
+                    queue.append((r, c))
+                    p -= 1       
+
   return grid
 
 # Generate a map with 5 rows, 5 columns, and 3 regions
